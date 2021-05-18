75279688da7a55a90df8e6bb36871e835dcd1243
Christopher Haslerud <haslerud1@hotmail.com>
2021-05-18 21:15:15 +0200

    adding instrument test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# create user instance for each team member
+# save in variable
+# create 5 instrument instances
+
+require 'faker'
+
+puts 'Creating 15 instruments...'
+15.times do |i|
+  instrument = Instrument.create!(
+    name: Faker::Games::Zelda.character,
+    model: Faker::Company.catch_phrase,
+    category: Faker::Music.instrument,
+    details: Faker::Company.catch_phrase,
+    year: rand(1950..2021)
+    rate: (rand(0.1) * 10).round(2),
+    owner_id: User.find(id = rand(1..3))
+  )
+  puts "#{i + 1}. #{instrument.name}"
+end
+puts 'Finished!'
