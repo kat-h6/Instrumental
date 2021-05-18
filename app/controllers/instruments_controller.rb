5b4723d432c589dece056c9982b99f18b20a1f61
Sepideh S <75858394+Sapideh1071@users.noreply.github.com>
2021-05-18 20:53:28 +0200

    list an instrument for rent-story

---
+++ app/controllers/instruments_controller.rb
@@ -0,0 +1,21 @@
+class InstrumentsController < ApplicationController
+	def new
+		@instrument = Instrument.new
+	end
+
+	def create
+		@instrument = Instrument.new(instrument_params)
+		if @instrument.save
+			redirect_to instrument_path(@instrument)
+		else
+			render :new
+		end
+	end
+
+  private 
+
+  def instrument_params
+  	params.require(:instrument).permit(:name, :model, :details, :year, :rate)
+  end
+end
+
