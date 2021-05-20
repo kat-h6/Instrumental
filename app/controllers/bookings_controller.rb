f5d5b81418f91c8296aa1f7c7f9e7ca52533c3a7
Sepideh S <75858394+Sapideh1071@users.noreply.github.com>
2021-05-20 17:21:23 +0200

    fixed some style issues in the controllers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,22 +1,22 @@
 class BookingsController < ApplicationController
 
-	def new
-		@booking = Booking.new
-	end
+  def new
+    @booking = Booking.new
+  end
 
-	def create
-	@booking = Booking.new(booking_params)
-	if @booking.save
-	  redirect_to instrument_path(@booking.instrument)
-	else
-	  render :new
-	end
-   end
+  def create
+    @booking = Booking.new(booking_params)
+    @instrument.user_id = current_user.id
+    if @booking.save
+      redirect_to instrument_path(@booking.instrument)
+    else
+      render :new
+    end
+  end
 
-   private
+  private
 
-   def booking_params
-   	  params.require(:booking).permit(:price, :start_date, :end_date, :details)
-   end
-
+  def booking_params
+    params.require(:booking).permit(:price, :start_date, :end_date, :details)
+  end
 end
--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,5 +1,5 @@
 class InstrumentsController < ApplicationController
-  
+
   def index
     @instruments = Instrument.all
   end
@@ -7,21 +7,21 @@ class InstrumentsController < ApplicationController
   def show
     set_instrument
   end
-  
-	def new
-		@instrument = Instrument.new
-	end
 
-	def create
-		@instrument = Instrument.new(instrument_params)
+  def new
+    @instrument = Instrument.new
+  end
+
+  def create
+    @instrument = Instrument.new(instrument_params)
     @instrument.owner_id = current_user.id
-		if @instrument.save
-			redirect_to instrument_path(@instrument)
-		else
-			render :new
-		end
-	end
-  
+    if @instrument.save
+      redirect_to instrument_path(@instrument)
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_instrument
@@ -29,8 +29,6 @@ class InstrumentsController < ApplicationController
   end
 
   def instrument_params
-  	params.require(:instrument).permit(:name, :model, :details, :year, :rate, :category, :photo)
+    params.require(:instrument).permit(:name, :model, :details, :year, :rate, :category, :photo)
   end
 end
-
-
