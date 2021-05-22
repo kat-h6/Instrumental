26c339854885f089cbabcf4b26e934a43db4a55a
Katharine Hao <hao.katharine@gmail.com>
2021-05-22 13:37:39 +0300

    cleaning, coming back in a sec

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   resources :instruments, only: [:index, :show, :new, :create] do
     resources :bookings, only: [:new, :index]
   end
+  resources :bookings, only: [:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'orders', to: 'dashboards#orders'
-  get 'rentals', to: 'dashboards#rentals'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -26,10 +26,9 @@ photos = ["https://source.unsplash.com/XIe1b4k9-k0/200x200",
           "https://source.unsplash.com/TRy6ZrPzDkM/200x200"
         ]
 
-puts 'Creating 15 instruments...'
-puts "added a change"
-15.times do
-  instrument = Instrument.new(
+file = URI.open('https://images.unsplash.com/photo-1558556405-3071bd8d3ee6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2736&q=80')
+
+instrument = Instrument.new(
     name: Faker::Music.instrument,
     model: Faker::Company.duns_number,
     category: ['woodwinds', 'brass', 'strings', 'percussion'].sample,
@@ -38,13 +37,29 @@ puts "added a change"
     rate: (rand(0.1) * 10).round(2),
     owner: User.all.sample,
     address: Faker::Address.full_address,
-    # photo: Cloudinary::Uploader.upload(photos.sample)
-  )
-  puts "new instrument"
-  instrument.photo.attach(URI.open(photos.sample))
-  puts "photo attached"
-  instrument.save
-  puts "saved"
-end
+    photo: Cloudinary::Uploader.upload(photos.sample)
+)
+instrument.photo.attach(io: file, filename: 'temp.png', content_type: 'image/png')
+instrument.save!
+# puts 'Creating 15 instruments...'
+# puts "added a change"
+# 15.times do
+#   instrument = Instrument.new(
+#     name: Faker::Music.instrument,
+#     model: Faker::Company.duns_number,
+#     category: ['woodwinds', 'brass', 'strings', 'percussion'].sample,
+#     details: Faker::Quote.famous_last_words,
+#     year: rand(1950..2021),
+#     rate: (rand(0.1) * 10).round(2),
+#     owner: User.all.sample,
+#     address: Faker::Address.full_address,
+#     photo: Cloudinary::Uploader.upload(photos.sample)
+#   )
+#   puts "new instrument"
+#   instrument.photo.attach(URI.open(photos.sample))
+#   puts "photo attached"
+#   instrument.save
+#   puts "saved"
+# end
 
 puts 'Finished!'
