22cf4ff686df6eb04dd71dddd2aae621e02e725d
Christopher Haslerud <haslerud1@hotmail.com>
2021-05-22 16:30:25 +0200

    changing routes file

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,12 @@ Rails.application.routes.draw do
   get '/user' => "instruments#index", :as => :user_root
 
   resources :instruments, only: [:index, :show, :new, :create] do
-    resources :bookings, only: [:new, :index, :create] do
-      resources :reviews, only: [ :new, :create ]
-    end
+    resources :bookings, only: [:new, :index, :create]
   end
 
-  resources :bookings, only: [:update]
+  resources :bookings, only: [:update] do
+    resources :reviews, only: [ :new, :create ]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
