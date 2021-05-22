2eec793a8212aa27fe22d6670b0eb2180c5bcf2e
Katharine Hao <hao.katharine@gmail.com>
2021-05-22 12:31:49 +0300

    cleaning

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,18 +18,26 @@ User.create(email: "kathy@gmail.com", password: "password")
 User.create(email: "tavis@gmail.com", password: "password")
 User.create(email: "Sepideh@gmail.com", password: "password")
 User.create(email: "chris@gmail.com", password: "password")
+photos = ["https://source.unsplash.com/XIe1b4k9-k0/200x200",
+          "https://source.unsplash.com/uqKyeMaaAOQ/200x200",
+          "https://source.unsplash.com/fTPSm7KD_d0/200x200",
+          "https://source.unsplash.com/rCca942E_sI/200x200",
+          "https://source.unsplash.com/TRy6ZrPzDkM/200x200"
+        ]
 
 puts 'Creating 15 instruments...'
 puts "added a change"
 15.times do
   instrument = Instrument.create!(
-    name: Faker::Games::Zelda.character,
+    name: Faker::Music.instrument,
     model: Faker::Company.duns_number,
-    category: Faker::Music.instrument,
+    category: ['woodwinds', 'brass', 'strings', 'percussion'].sample,
     details: Faker::Quote.famous_last_words,
     year: rand(1950..2021),
     rate: (rand(0.1) * 10).round(2),
-    owner: User.all.sample
+    owner: User.all.sample,
+    address: Faker::Address.full_address,
+    photo: Cloudinary::Uploader.upload(photos.sample)
   )
 end
 puts 'Finished!'
